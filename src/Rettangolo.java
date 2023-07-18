103318b3a6637371673f5461c5b1571302d915df
Burdusa Daniel <danielburdusa26@gmail.com>
2023-07-18 14:48:19 +0200

    finish

--- src/Rettangolo.java
+++ src/Rettangolo.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Rettangolo {
+public class Rettangolo {
+    int altezza;
+    int base;
+    public Rettangolo(){
+        altezza=0;
+        base=0;
+    }
+    public int perimetro(int h, int b){
+        int perimetro = (h*2)+(b*2);
+        return perimetro;
+    }
+    public int area (int h , int b){
+        int area = b*h;
+        return area;
+    }
 }
--- src/tester.java
+++ src/tester.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class tester {
+import java.util.Scanner;
+
+public class tester {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Rettangolo pippo= new Rettangolo();
+        System.out.println("dammi la base del rettangolo");
+        int a = input.nextInt();
+        System.out.println("dammi l'altezza del rettangolo");
+        int b = input.nextInt();
+        int perimetro = pippo.perimetro(a,b);
+        System.out.println("il perimetro del rettangolo è : "+perimetro);
+        int area = pippo.area(a,b);
+        System.out.println("l'area del rettangolo è : "+ area);
+    }
 }
